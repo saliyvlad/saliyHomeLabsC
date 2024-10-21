f31899857fb2f53fac3f4873a96c00fa24d2a015
saliyvlad <saliy.vlad.jr@gmail.com>
2024-10-21 14:44:44 +0700

    Update prac3var4.c

--- lab3/prac3var4.c
+++ lab3/prac3var4.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 void main(){//Программа вычисляет простое число  
 	int a,n;
-	for (a=5,n = 2; n<a && a%n!=0;n++);
+	puts("введите число для проверки:");
+	scanf("%d",&a);
+	for (n = 2; n<a && a%n!=0;n++);
 {	
-	if (n==a) puts("1");
+	if (n==a) puts("простое число");
 	else puts("0");
 }
 }
