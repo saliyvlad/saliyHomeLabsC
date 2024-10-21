857f7cabd3983e5ae6df4f0cdb7426abf5612740
saliyvlad <saliy.vlad.jr@gmail.com>
2024-10-21 14:55:44 +0700

    Update prac3var5.c

--- lab3/prac3var5.c
+++ lab3/prac3var5.c
@@ -2,10 +2,10 @@
 void main(){//Программа проверяет на четность
 	int n,a,s;
 	scanf("%d",&a);
-	for (int s=0,n = 2; n<a;n++){
-	if(a%n==0)s++;
+	for (int s=0,n = 2; n<=a;n++){
+	if(a%n==0 || a == 2)s++;
 
-	if (s==0) puts("1");
-	else puts("0");
+	if (s==0){puts("1");break;}
+	else {puts("0");break;}
 }
 }
