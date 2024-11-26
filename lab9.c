d9fbb4818544542b9ea96ecbb5aaf3760c2c4dd3
saliyvlad <saliy.vlad.jr@gmail.com>
2024-11-26 10:44:57 +0300

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -407,3 +407,125 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void fillMatrix(int **matrix, int rows, int cols, int random);
+void sortColumn(int **matrix, int rows, int col);
+int countPositive(int **matrix, int rows, int col);
+void replaceColumnWithZeros(int **matrix, int rows, int col);
+void printMatrix(int **matrix, int rows, int cols);
+
+int main() {
+    int rows, cols, i, j;
+    int random;
+
+    printf("Введите количество строк и столбцов матрицы: ");
+    scanf("%d %d", &rows, &cols);
+
+    // Проверка на ввод только 0 или 1
+    do {
+        printf("Заполнить матрицу случайными числами? (1 - да, 0 - нет): ");
+        scanf("%d", &random);
+    } while (random != 0 && random != 1);
+
+    int **matrix = (int **)malloc(rows * sizeof(int *));
+    for (i = 0; i < rows; i++) {
+        matrix[i] = (int *)malloc(cols * sizeof(int));
+    }
+
+    fillMatrix(matrix, rows, cols, random);
+    printf("Исходная матрица:\n");
+    printMatrix(matrix, rows, cols);
+
+    int minSwapsCol = 0;
+    int minSwaps = rows * cols; // Начальное большое значение
+
+    for (j = 0; j < cols; j++) {
+        sortColumn(matrix, rows, j);
+        int positiveCount = countPositive(matrix, rows, j);
+        int swaps = countPositive(matrix, rows, j); // Подсчет перестановок (здесь нужно уточнить логику)
+        
+        if (swaps < minSwaps) {
+            minSwaps = swaps;
+            minSwapsCol = j;
+        }
+    }
+
+    printf("Матрица после сортировки:\n");
+    printMatrix(matrix, rows, cols);
+
+    replaceColumnWithZeros(matrix, rows, minSwapsCol);
+    printf("Матрица после замены столбца %d на нули:\n", minSwapsCol);
+    printMatrix(matrix, rows, cols);
+
+    for (i = 0; i < rows; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+
+    return 0;
+}
+
+void fillMatrix(int **matrix, int rows, int cols, int random) {
+    if (random) {
+        srand(time(NULL));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = rand() % 100 - 50; // Числа от -50 до 49
+            }
+        }
+    } else {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                printf("Введите элемент [%d][%d]: ", i, j);
+                scanf("%d", &matrix[i][j]);
+            }
+        }
+    }
+}
+
+void sortColumn(int **matrix, int rows, int col) {
+    for (int i = 0; i < rows - 1; i++) {
+        for (int j = i + 1; j < rows; j++) {
+            if (matrix[i][col] > matrix[j][col]) {
+                int temp = matrix[i][col];
+                matrix[i][col] = matrix[j][col];
+                matrix[j][col] = temp;
+            }
+        }
+    }
+}
+
+int countPositive(int **matrix, int rows, int col) {
+    int count = 0;
+    for (int i = 0; i < rows; i++) {
+        if (matrix[i][col] > 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+void replaceColumnWithZeros(int **matrix, int rows, int col) {
+    for (int i = 0; i < rows; i++) {
+        matrix[i][col] = 0;
+    }
+}
+
+void printMatrix(int **matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
