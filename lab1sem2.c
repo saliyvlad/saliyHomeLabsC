fe494d193313664da902b05e18cb90a6702eb839
saliyvlad <saliy.vlad.jr@gmail.com>
2025-02-18 04:31:13 +0300

    Update lab1sem2.c

--- lab1sem2.c
+++ lab1sem2.c
@@ -106,4 +106,17 @@ int main() {
 
     return 0;
 }
-
+void selectionSort(struct Student arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i; // Индекс минимального элемента
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j].average_score < arr[min_idx].average_score) {
+                min_idx = j; // Обновляем индекс минимального элемента
+            }
+        }
+        // Меняем местами найденный минимальный элемент и текущий элемент
+        struct Student temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
